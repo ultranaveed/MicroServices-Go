54f00745c20cd17baa4c1dca8e3e8aa70bfbe94d
ultranaveed <naveednovo01@gmail.com>
2025-08-02 23:45:14 +0600

    Started mail-service

---
+++ mail-service/cmd/api/mailer.go
@@ -0,0 +1,26 @@
+package main
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Attachments []string
+	Data        any
+	DataMap     map[string]any
+}
+
+func (m *Mail) SendSMTPMessage(msg Message) error {
+
+}
