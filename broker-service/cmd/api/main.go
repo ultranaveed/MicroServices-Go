71fbadc16abe539b9c5392c62dc25f10b05a17fc
ultranaveed <naveednovo01@gmail.com>
2025-08-08 23:54:59 +0600

    listener microservice added

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const webPort = "80"
 
-type Config struct{}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	// try to connect to rabbitmq
+	rabbitCon, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitCon.Close()
+
+	app := Config{
+		Rabbit: rabbitCon,
+	}
 
 	log.Printf("Starting broker service on %s\n", webPort)
 
@@ -21,10 +38,40 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		log.Panic(err)
 	}
 
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ!")
+			connection = c
+			break
+		}
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
--- broker-service/go.mod
+++ broker-service/go.mod
@@ -5,4 +5,5 @@ go 1.24.5
 require (
 	github.com/go-chi/chi/v5 v5.2.2 // indirect
 	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
 )
