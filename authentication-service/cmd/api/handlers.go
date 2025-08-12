f7671f036f766985c4ebaadb1c420e8c1d1e5fea
ultranaveed <naveednovo01@gmail.com>
2025-08-12 12:34:53 +0600

    rabbitmq done.

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -58,7 +58,7 @@ func (app *Config) logRequest(name, data string) error {
 	entry.Name = name
 	entry.Data = data
 
-	jsonData, _ := json.MarshalIndent(entry, "", "/t")
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
 	logServiceURL := "http://logger-service/log"
 
 	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
